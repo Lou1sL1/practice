c3966adc08d377b295318b7e645a5003474a1c4c
Lou1sL1 <lijunhong2022@outlook.com>
2026-06-07 00:31:27 +0800

    2026.06.06

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+class Animal{
+private:
+    std::string name;
+    std::string var;
+    int amount;
+public:
+    Animal(std::string name,std::string var,int amount):name(name),var(var),amount(amount){
+        std::cout<<"Initialized."<<std::endl;
+    }
+};
